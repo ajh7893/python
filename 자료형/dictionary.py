653144a9c9b7f2f0ec50a2ded0d7adfa84fd9fdb
ajh <jhan@ntro.kr>
2024-12-17 15:02:58 +0900

    update

--- 자료형/dictionary.py
+++ 자료형/dictionary.py
@@ -29,3 +29,23 @@ fruits_list = list(fruits.values())
 print(fruits_list)
 print(sum(fruits_list))
 print(sum(fruits_list)/len(fruits_list))
+
+# 각점수는 국 영 수 순으로 한다. 
+score = {"가은":[82,97,88], "나영":[92,87,82],"다래":[84,77,94]}
+#학생별 평균. 
+score1 =score["가은"]
+print(sum(score1)/len(score1))
+score2 =score["나영"]
+print(sum(score2)/len(score2))
+score3 =score["다래"]
+print(sum(score3)/len(score3))
+
+# 각 과목별  평균
+score_list = list(score.values())
+kor = score_list[0][0] + score_list[1][0] + score_list[2][0]
+eng = score_list[0][1] + score_list[1][1] + score_list[2][1]
+mat = score_list[0][2] + score_list[1][2] + score_list[2][2]
+
+print(kor/3)
+print(eng/3)
+print(mat/3)
