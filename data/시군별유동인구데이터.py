6a73b5dbcfa5bc6732bee18e4cb54eff35795028
ajh <jhan@ntro.kr>
2024-12-19 15:48:32 +0900

    how to use data

--- data/시군별유동인구데이터.py
+++ data/시군별유동인구데이터.py
@@ -27,4 +27,23 @@ for col in df.columns:
 print("\n=== 데이터 미리보기 ===")
 print(df.head())
 print(df)
-data_array = df.to_numpy()
+
+#기초 통계 산출하기 
+numOfVisitors = []
+originNames =[]
+
+for i in range(len(df)):  # 85104개의 행을 순회
+    if '202012' in str(df.iloc[i, 0]) and '경기도 안양시' in str(df.iloc[i, 2]):
+        numOfVisitors.append(float(df.iloc[i,3]))
+        originNames.append(df.iloc[i,1])
+
+print(numOfVisitors)
+print(originNames)
+
+print(originNames[numpy.argmin(numOfVisitors)])
+print(numpy.min(numOfVisitors))
+print(originNames[numpy.argmax(numOfVisitors)])
+print(numpy.max(numOfVisitors))
+print(numpy.sum(numOfVisitors))
+print(numpy.average(numOfVisitors))
+print(numpy.std(numOfVisitors))
